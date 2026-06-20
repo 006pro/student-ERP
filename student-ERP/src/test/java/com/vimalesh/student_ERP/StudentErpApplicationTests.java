c59672ec0ff3fe2a1d2dcfb98d4833a4e060451f
Vimalesh rk <rkvimalesh052@gmail.com>
2026-06-20 20:37:40 +0530

    Initial commit

---
+++ student-ERP/src/test/java/com/vimalesh/student_ERP/StudentErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vimalesh.student_ERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

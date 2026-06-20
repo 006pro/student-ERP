edcff3cc635c55776ee0e3698297d11f6c4a645f
Vimalesh rk <rkvimalesh052@gmail.com>
2026-06-20 21:09:24 +0530

    student and user entity

---
+++ student-ERP/src/main/java/entity/Student.java
@@ -0,0 +1,28 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String rollNo;
+    private LocalDate dob;
+    private String parentContact;
+    private String address;
+    private String photoUrl;
+
+
+
+}
+
+
